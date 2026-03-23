5ec9295ed4be462e65d08cc6435460376ee491b9
skyz67-88 <albertojosealeman@gmail.com>
2026-03-23 12:33:03 +0100

    neuer commit war halt delu
    unc

--- time_cfg.py
+++ time_cfg.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def get_time():
     now = datetime.now()
-    day_month = now.strftime("%m-%d")
+    day_month = now.strftime("%d-%m")
     hour_minute = now.strftime("%H:%M")
     return day_month, hour_minute
